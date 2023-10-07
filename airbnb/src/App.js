04d7c427e86e62e52758307b6953c48653e519eb
Siddharth Singh <sidsingh264@gmail.com>
2023-10-07 16:31:36 +0530

    airbnb done

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -3,21 +3,25 @@ import Navbar from './components/Navbar';
 import GridImage from './components/GridImage';
 import Experiences from './components/Experiences';
 import Card from './components/Card';
+import data from './assets/data';
 
 function App() {
+  const cardElements = data.map((card)=> {
+    return( 
+    <Card 
+    key = {card.id}
+    card = {card}      
+    />)
+  })
   return (
     <div className="App">
       <Navbar/>
       <GridImage/>
       <Experiences/>
-      <Card
-        img = 'Katie.png'
-        rating = {5}
-        numOfReviews = {6}
-        country = 'USA'
-        title = 'Life lessons with Katie Zaferes'
-        price = {100}  
-      />
+      <div className='cardssection'>
+        {cardElements}
+      </div>
+      
     </div>
   );
 }
--- airbnb/src/components/Card.jsx
+++ airbnb/src/components/Card.jsx
@@ -1,29 +1,40 @@
-// import Katie from '../assets/Katie.png'
 import Star from '../assets/star.png'
 import Ellipse from '../assets/Ellipse.png'
 
 export default function Card(props){
+    let badgeText
+    if(props.card.openSpots === 0){
+        badgeText = 'SOLD OUT'
+    } else if(props.card.location === 'Online'){
+        badgeText = 'ONLINE'
+    }
     console.log(props)
     return (
         <div className="card-container">
-            <div className="cardImg">
-                <img src={props.img} alt=""  />
-            </div>
-            <div className="cardReviews">
-                <img className='cardStar' src={Star} alt="" />
-                <span>{props.rating}</span>
-                <span>({props.numOfReviews})</span>
-                <img className = "cardEl" src={Ellipse} alt="" />
-                <span>{props.country}</span>
-            </div>
+           
+                {badgeText &&
+                 <div className="card-badge"> 
+                <span>{badgeText}</span> 
+                </div>}
 
-            <div className="cardDetails">
-                <div className="desc">
-                {props.title}
+
+            <img className = "cardImg" src={`./images/${props.card.coverImg}`} alt=""  />
+            <div className="cardAllDetails">
+                <div className="cardReviews">
+                    <img className='cardStar' src={Star} alt="" />
+                    <span>{props.card.stats.rating}</span>
+                    <span>({props.card.stats.reviewCount})</span>
+                    <img className = "cardEl" src={Ellipse} alt="" />
+                    <span>{props.card.location}</span>
                 </div>
-                <div className="rate">
-                    <span className='boldtext'>From $ {props.price}</span>
-                    <span className='simpletext'>/ Person</span>
+                <div className="cardDetails">
+                    <div className="desc">
+                    {props.card.title}
+                    </div>
+                    <div className="rate">
+                        <span className='boldtext'>From $ {props.card.price}</span>
+                        <span className='simpletext'>/ Person</span>
+                    </div>
                 </div>
             </div>
         </div>
