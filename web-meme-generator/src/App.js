e149826ca5f88fba4936fcaa4ec91130c8b3206a
Siddharth Singh <sidsingh264@gmail.com>
2023-10-16 21:51:36 +0530

    Completed meme-generator personal-version

--- web-meme-generator/src/App.js
+++ web-meme-generator/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from "./components/Header";
-import Form from './components/Form';
 import Meme from './components/Meme';
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Form/>
       <Meme />
     </div>
   );
--- web-meme-generator/src/components/Meme.jsx
+++ web-meme-generator/src/components/Meme.jsx
@@ -1,24 +1,59 @@
-import Memes from "../assets/memesData"
+import React from 'react';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Memes from '../assets/memesData'
 
-const Meme = () => {
+export default function Form() {
+  const [url, setUrl] = React.useState(Memes.data.memes[57].url)
+  const [toptext, setTopText] = React.useState("Shut Up")
+  const [bottomText, setBottomText] = React.useState("And Take My Money")
   function getRandomMeme() {
-    // write code here
     const randomIndex = Math.floor(Math.random() *100)
-    console.log(Memes.data.memes[randomIndex].name)
-    return Memes.data.memes[randomIndex].url
+    setUrl(Memes.data.memes[randomIndex].url)
   }
-  const url = getRandomMeme();
+  
+
   return (
-    <div className="meme-container">
+    <div className="form-container">
+      <div className="box-container">
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1.5, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField value={toptext} onChange={(e)=> setTopText(e.target.value)} className="form-input" id="outlined-basic" variant="outlined" placeholder="Top-text"/>
+      </Box>
+
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1.5, width: "25ch" },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField value={bottomText} onChange={(e)=> setBottomText(e.target.value)} className="form-input" id="outlined-basic" variant="outlined" placeholder="Bottom text"/>
+      </Box>
+      </div>
+
+      <div className="submit-button">
+        <Button onClick={getRandomMeme} variant="contained">Get a new Meme Image 🖼️</Button>
+      </div>
+
+
+      <div className="meme-container">
       <img src={url} alt="" />
       <div className="text1">
-        <span>Shut Up</span>
+        <span>{toptext}</span>
       </div>
       <div className="text2">
-        <span>And Take My Money</span>
+        <span>{bottomText}</span>
       </div>
     </div>
+    </div>
   );
-};
-
-export default Meme;
+}
